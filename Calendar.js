6ce77af71ff4b8e702d7b5aa0d0b11be587d730c
Ryan Steiner <steinerr3@bzz.ch>
2024-08-05 17:11:09 +0200

    Setup Tage

--- Calendar.js
+++ Calendar.js
@@ -2,6 +2,7 @@ export class Calendar {
     constructor() {}
     setup () {
         this.setupTimes();
+        this.setupDays();
     }
 
     setupTimes() {
@@ -15,5 +16,39 @@ export class Calendar {
                 .appendTo(slots);
         }
         $('.dayTime').append(header).append(slots);
+    }
+
+    setupDays() {
+        const cal = this;
+        $('.day').each(function () {
+            const dayIndex = parseInt($(this).attr('data-dayIndex'));
+            const name = $(this).attr('data-name');
+            const header = $('<div></div>').addClass('colummHeader').text(name);
+            const slots = $('<div></div>').addClass('slots');
+            for (let hour = 0; hour < 24; hour++) {
+                $('<div></div>')
+                    .attr('data-hour',hour)
+                    .addClass('slot')
+                    .appendTo(slots)
+                    .click(( ) => cal.clickSlot(hour, dayIndex))
+                    .hover(
+                        () => cal.hoverOver(hour),
+                        () => cal.hoverOut()
+                    );
+            }
+            $(this).append(header).append(slots);
+        });
+    }
+
+    clickSlot(hour, dayIndex){
+        console.log('click!', hour, dayIndex);
+    }
+
+    hoverOver(hour) {
+        console.log('hover!', hour);
+    }
+
+    hoverOut(){
+        //todo
     }
 }
