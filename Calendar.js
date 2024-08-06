eed4ac5427b45732a82d7a26ef8d6b98b113cf1f
Ryan Steiner <steinerr3@bzz.ch>
2024-08-06 17:45:54 +0200

    Anzeige Woche

--- Calendar.js
+++ Calendar.js
@@ -1,8 +1,16 @@
+import {getDayIndex, addDays } from "./helper.js";
+
 export class Calendar {
-    constructor() {}
-    setup () {
+    constructor() {
+        this.weekStart = null;
+        this.weekEnd = null;
+    }
+
+    setup() {
         this.setupTimes();
         this.setupDays();
+        this.calculateCurrentWeek();
+        this.showWeek();
     }
 
     setupTimes() {
@@ -10,9 +18,9 @@ export class Calendar {
         const slots = $('<div></div>').addClass('slots');
         for (let hour = 0; hour < 24; hour++) {
             $('<div></div>')
-                .attr('data-hour',hour)
+                .attr('data-hour', hour)
                 .addClass('time')
-                .text(`${hour}:00 - ${hour+1}:00`)
+                .text(`${hour}:00 - ${hour + 1}:00`)
                 .appendTo(slots);
         }
         $('.dayTime').append(header).append(slots);
@@ -24,13 +32,14 @@ export class Calendar {
             const dayIndex = parseInt($(this).attr('data-dayIndex'));
             const name = $(this).attr('data-name');
             const header = $('<div></div>').addClass('columnHeader').text(name);
+            $('<div></div>').addClass('dayDisplay').appendTo(header);
             const slots = $('<div></div>').addClass('slots');
             for (let hour = 0; hour < 24; hour++) {
                 $('<div></div>')
-                    .attr('data-hour',hour)
+                    .attr('data-hour', hour)
                     .addClass('slot')
                     .appendTo(slots)
-                    .click(( ) => cal.clickSlot(hour, dayIndex))
+                    .click(() => cal.clickSlot(hour, dayIndex))
                     .hover(
                         () => cal.hoverOver(hour),
                         () => cal.hoverOut()
@@ -40,7 +49,7 @@ export class Calendar {
         });
     }
 
-    clickSlot(hour, dayIndex){
+    clickSlot(hour, dayIndex) {
         console.log('click!', hour, dayIndex);
     }
 
@@ -48,7 +57,31 @@ export class Calendar {
         console.log('hover!', hour);
     }
 
-    hoverOut(){
+    hoverOut() {
         //todo
+    }
+
+    calculateCurrentWeek() {
+        const now = new Date();
+        this.weekStart = addDays(now, -getDayIndex(now));
+        this.weekEnd = addDays(this.weekStart, 6);
+    }
+
+    showWeek() {
+        const options = {
+            month: '2-digit',
+            day: '2-digit',
+            year: 'numeric',
+        }
+        $('#weekStartDisplay').text(this.weekStart.toLocaleDateString(undefined, options));
+        $('#weekEndDisplay').text(this.weekEnd.toLocaleDateString(undefined, options));
+        for (let dayIndex = 0; dayIndex < 7; dayIndex++) {
+            const date = addDays(this.weekStart, dayIndex);
+            const display = date.toLocaleDateString(undefined, {
+                month: '2-digit',
+                day: '2-digit',
+            });
+            $(`.day[data-dayIndex=${dayIndex}] .dayDisplay`).text(display);
+        }
     }
 }
